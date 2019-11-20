f66909b842590e8266c3eaadcfef3f50e91b7f7b
Søren Spangsberg Jørgensen <smsj@easv.dk>
2019-11-20 09:57:12 +0100

    Initial import

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Store a new user.
+     * 
+     */
+}
